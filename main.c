22f62dda719fb92f55f72d40193f0119d8683160
sandeepa <sansamu2004@gmail.com>
2025-10-14 10:21:49 +0530

    inisialized the project

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int choice;
+
+    printf("1. City management\n");
+    printf("2. Distance management\n");
+    printf("3. Vehicale Management\n");
+    printf("4. Delivery Request Handling\n");
+    printf("5. Cast,Time and Fuel Calculation\n");
+    printf("6. Delivary Records\n");
+    printf("7. Finding the Least-cost Route (Least-Distance)\n");
+    printf("8. Performance Reports\n");
+    printf("9. exit\n");
+
+    switch(choice)
+    {
+    case 1:
+
+
+
+
+    }
+    return 0;
+}
